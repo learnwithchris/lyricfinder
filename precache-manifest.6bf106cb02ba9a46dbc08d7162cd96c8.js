5f0ea74b02fea8d871b355030f034f09019d7b1f
learnwithchris <cegan35@gmail.com>
2020-03-31 08:14:07 -0400

    Updates

---
+++ precache-manifest.6bf106cb02ba9a46dbc08d7162cd96c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa69ba2eccdf0813d07b9e71f090424",
+    "url": "/lyricfinder/index.html"
+  },
+  {
+    "revision": "2b406cb17bdf870af569",
+    "url": "/lyricfinder/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "29e765fa810023f23458",
+    "url": "/lyricfinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2b406cb17bdf870af569",
+    "url": "/lyricfinder/static/js/2.e389fc68.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/lyricfinder/static/js/2.e389fc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e765fa810023f23458",
+    "url": "/lyricfinder/static/js/main.7c3c6a35.chunk.js"
+  },
+  {
+    "revision": "118e857c0dbea767e859",
+    "url": "/lyricfinder/static/js/runtime-main.b6ab8e1f.js"
+  }
+]);
